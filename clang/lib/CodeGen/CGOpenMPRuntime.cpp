f31da7100b86775df58c177cc90735a82963a5ad
Pranav Bhandarkar <pranav.bhandarkar@amd.com>
2024-06-20 14:33:18 -0500

    more debug prints

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -4514,9 +4514,10 @@ void CGOpenMPRuntime::emitTaskCall(CodeGenFunction &CGF, SourceLocation Loc,
                                    const OMPTaskDataTy &Data) {
   if (!CGF.HaveInsertPoint())
     return;
-
+  LLVM_DEBUG(llvm::dbgs() << "**InsertPoint before emitTaskInit***\n" << *CGF.Builder.GetInsertBlock());
   TaskResultTy Result =
       emitTaskInit(CGF, Loc, D, TaskFunction, SharedsTy, Shareds, Data);
+  LLVM_DEBUG(llvm::dbgs() << "**InsertPoint after emitTaskInit***\n" << *CGF.Builder.GetInsertBlock());
   llvm::Value *NewTask = Result.NewTask;
   llvm::Function *TaskEntry = Result.TaskEntry;
   llvm::Value *NewTaskNewTaskTTy = Result.NewTaskNewTaskTTy;
@@ -9707,6 +9708,7 @@ static void emitTargetCallKernelLaunchNew(
         DeviceID, RTLoc, AllocaIP));
   };
 
+  LLVM_DEBUG(llvm::dbgs() << "calling function before targettaskbaseddirective is"  << *CGF.Builder.GetInsertBlock());
   if (RequiresOuterTask) {
     if (NewClangTargetTaskCodeGen) {
       llvm::errs() << "Using OMPIRBuilder for target task codegen\n";
