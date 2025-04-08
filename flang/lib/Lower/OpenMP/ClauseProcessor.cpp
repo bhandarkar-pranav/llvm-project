c313ba0120db2413a1a41b76aaa85529a0762d94
Pranav Bhandarkar <pranav.bhandarkar@amd.com>
2025-04-08 17:18:50 -0500

    clean up som more

--- flang/lib/Lower/OpenMP/ClauseProcessor.cpp
+++ flang/lib/Lower/OpenMP/ClauseProcessor.cpp
@@ -1047,6 +1047,7 @@ void ClauseProcessor::processMapObjects(
     llvm::SmallVector<mlir::Value> bounds;
     std::stringstream asFortran;
     std::optional<omp::Object> parentObj;
+
     fir::factory::AddrAndBoundsInfo info =
         lower::gatherDataOperandAddrAndBounds<mlir::omp::MapBoundsOp,
                                               mlir::omp::MapBoundsType>(
@@ -1081,6 +1082,7 @@ void ClauseProcessor::processMapObjects(
                                                     mapperIdName)
                      : mlir::FlatSymbolRefAttr();
     }
+
     // Explicit map captures are captured ByRef by default,
     // optimisation passes may alter this to ByCopy or other capture
     // types to optimise
